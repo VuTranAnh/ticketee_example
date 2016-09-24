87af4c46d2e1ae43118bea1195500cad515b8106
trananhvu <eyelight93@gmail.com>
2016-09-25 02:45:12 +0700

    Implement creating tickets for a project

---
+++ spec/features/creating_tickets_spec.rb
@@ -0,0 +1,37 @@
+require "rails_helper"
+
+RSpec.feature "Users can create new ticket" do
+
+  before do
+    project = FactoryGirl.create(:project, name: "Internet Explorer")
+
+    visit project_path(project)
+    click_link "New Ticket"
+  end
+
+  scenario "with valid attributes" do
+    fill_in "Name", with: "Non-standards compliance"
+    fill_in "Description", with: "My pages are ugly!"
+    click_button "Create Ticket"
+
+    expect(page).to have_content "Ticket has been created."
+  end
+
+  scenario "With invalid attributes" do
+    click_button "Create Ticket"
+
+    expect(page).to have_content "Ticket has not been created."
+    expect(page).to have_content "Name can't be blank"
+    expect(page).to have_content "Description can't be blank"
+  end
+
+  scenario "Description must be at least 10 characters" do
+    fill_in "Name", with: "Non-standards compliance"
+    fill_in "Description", with: "123456789"
+    click_button "Create Ticket"
+
+    expect(page).to have_content "Ticket has not been created."
+    expect(page).to have_content "Description is too short"
+  end
+
+end
